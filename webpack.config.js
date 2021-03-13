263a8a3b6340ee59c384dbc069ba76d8e1ec13e4
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2021-03-13 07:40:37 -0500

    added new folders restart

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,68 @@
-// const path = require("path");
-// const webpack = require("webpack");
-// require("bootstrap");
-
-// //The entry point is the root of the bundle and the beginning of the dependency graph, so give it the relative path to the client's code. 
-// module.exports = {
-//     entry: './assets/js/script.js',
-//     //webpack will next take the entry point we have provided, bundle that code, and output that bundled code to a folder that we specify. It is common and best practice to put your bundled code into a folder named dist, which is short for distribution. Add the following code
-//     output: {path: path.resolve(__dirname, 'dist'), filename: 'main.budle.js'},
-    
-//     plugins: [
-//         new webpack.ProvidePlugin({
-//             $: "jquery",
-//             jQuery: "jquery"
-//         }),
-//     ],
-//     //The final piece of our basic setup will provide the mode in which we want webpack to run. By default, webpack wants to run in production mode. In this mode, webpack will minify our code for us automatically, along with some other nice additions. We want our code to run in development mode, so add the following code
-//     mode: 'development'
-
-// }
-
-const webpack = require("webpack")
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-module.exports = {
+
+const config = {
   entry: {
-    app: "./assets/js/script.js",
-    events: "./assets/js/events.js",
-    schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
   },
   output: {
-    filename: "[name].bundle.js",
-    path: __dirname + "/dist",
+    filename: '[name].bundle.js',
+    path: __dirname + '/dist'
   },
-  //In the config object, we added an object to the rules array. This object will identify the type of files to pre-process using the test property to find a regular expression, or regex. In our case, we are trying to process any image file with the file extension of .jpg. We could expand this expression to also search for other image file extensions such as .png, .svg, or .gif
   module: {
     rules: [
       {
-        test: /\.jpg$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name(file) {
-                return "[path][name].[ext]"
-              },
-              publicPath: function(url){
-                return url.replace("../", "assets/")
-              }
-            }
-          },
-          {
-            loader: 'image-webpack-loader'
-          }
-        ]
-      }
-    ]
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name (file) {
+              return '[path][name].[ext]'
+            },
+            publicPath: function(url) {
+                return url.replace('../', '/assets/')
+            },
+          }  
+        }, 
+        {
+          loader: 'image-webpack-loader',
+        },
+      ],
+      },
+    ],
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
+        $: "jquery",
+        jQuery: "jquery"
     }),
-    //Notice that when we added the BundleAnalyzerPlugin, we configured the analyzerMode property with a static value. This will output an HTML file called report.html that will generate in the dist folder.
-    //We can also set this value to disable to temporarily stop the reporting and automatic opening of this report in the browser.
     new BundleAnalyzerPlugin({
-      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+      analyzerMode: "static"
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [{
+        src: path.resolve("assets/img/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
     })
-
-  ],
+],
   mode: "development"
 };
+
+module.exports = config;
